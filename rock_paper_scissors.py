252e44d64d6804299819d602f68301b9a77ff46c
Dev-In-The-Z <drigg94j@gmail.com>
2025-10-20 03:22:38 -0500

    rock_paper_scissors_v2.1.py
    Added Rules and Score reset options

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,21 +1,37 @@
 import random #imports the random archive to randomize the computers choice
 
+print("Welcome to Rock, Paper, Scissors!")
+
+#rules for the game modes
+rules = "Classic mode:\nRock smashes Scissors\nPaper covers Rock\nScissors cut Paper\n\nRemix Mode:\nRock smashes scissors\nRock crushes Lizard\nPaper covers Rock\nPaper disproves Spock\nScissors cut Paper\nScissors decapitates Lizard\nLizard eats Paper\nLizard poisons Spock\nSpock vaporizes Rock\nSpock smashes Scissors"
+
+rules_classic = "Classic mode:\nRock smashes Scissors\nPaper covers Rock\nScissors cut Paper"
+
+rules_remix = "Remix Mode:\nRock smashes scissors\nRock crushes Lizard\nPaper covers Rock\nPaper disproves Spock\nScissors cut Paper\nScissors decapitates Lizard\nLizard eats Paper\nLizard poisons Spock\nSpock vaporizes Rock\nSpock smashes Scissors"
+
+
 def get_choices_classic(): #function to get both players choices for classic
     options = ["rock", "paper", "scissors"]
-    player_choice = input("Enter a choice (rock, paper, scissors):").strip().lower()
+    player_choice = input("Enter a choice (rock, paper, scissors, rules):").strip().lower()
+    while player_choice == "rules":
+        print(rules_classic)
+        player_choice = input("Enter a choice (rock, paper, scissors, rules):").strip().lower()
     while player_choice not in options: #checks if players input is within choices
         print("Invalid answer, please try again!")
-        player_choice = input("Enter a choice (rock, paper, scissors):").strip().lower()
+        player_choice = input("Enter a choice (rock, paper, scissors, rules):").strip().lower()
     computer_choice = random.choice(options)
     choices = {"player" : player_choice, "computer": computer_choice}
     return choices
     
 def get_choices_remix(): #function to get both players choices for remix
     options = ["rock", "paper", "scissors", "lizard", "spock"]
-    player_choice = input("Enter a choice (rock, paper, scissors, lizard, spock):").strip().lower()
+    player_choice = input("Enter a choice (rock, paper, scissors, lizard, spock, rules):").strip().lower()
+    while player_choice == "rules":
+        print(rules_remix)
+        player_choice = input("Enter a choice (rock, paper, scissors, lizard, spock, rules):").strip().lower()
     while player_choice not in options: #checks if players input is within choices
         print("Invalid answer, please try again!")
-        player_choice = input("Enter a choice (rock, paper, scissors, lizard, spock):").strip().lower()
+        player_choice = input("Enter a choice (rock, paper, scissors, lizard, spock, rules):").strip().lower()
     computer_choice = random.choice(options)
     choices = {"player" : player_choice, "computer": computer_choice}
     return choices
@@ -123,15 +139,29 @@ def check_win_remix(player, computer, player_win, computer_win): #function to ch
             computer_win += 1
             return "Paper disproves Spock. You lose!", player_win, computer_win
             
-version = input("Which version would you like to play? Classic or remix? ").strip().lower() #checks which version the player wants to play
+version = input("Which version would you like to play? \nClassic or remix? (Type 'rules' to display rules for both game modes) ").strip().lower() #checks which version the player wants to play
+while version == "rules":
+    print(rules)
+    version = input("Which version would you like to play? \nClassic or remix? (Type 'rules' to display rules for both game modes) ").strip().lower()
 while version != "classic" and version != "remix":
     print("Invalid input. Please try again.")
-    version = input("Which version would you like to play? Classic or remix? ").strip().lower()
+    version = input("Which version would you like to play? Classic or remix? Type 'rules' to display rules for both game modes)").strip().lower()
     
 if version == "classic": #plays classic mode
     play_again = "y"   
 
     while play_again == "y": #Loops game till player choses "n"
+    
+        if player_win > 0 or computer_win > 0: #asks if score wants to be reset
+	        reset = input("Would you like to reset the score? (y/n) ").strip().lower()
+	        while reset != "y" and reset != "n":
+		        print("Invalid response. Please try again.")
+		        reset = input("Would you like to reset the score? (y/n) ").strip().lower()
+	        if reset == "y":
+		        player_win = 0
+		        computer_win = 0
+		        print("Score Reset!")
+		        
         choices = get_choices_classic()
         result, player_win, computer_win = check_win_classic(choices["player"], choices["computer"], player_win, computer_win)
         print(result)
@@ -141,9 +171,20 @@ if version == "classic": #plays classic mode
                 print("Invalid input. Please try again.")
                 play_again = input("Play again? (y/n)").strip().lower()
 else: #plays remix version
-    play_again = "y"   
+    play_again = "y"
 
     while play_again == "y": #Loops game till player choses "n"
+    
+        if player_win > 0 or computer_win > 0: #asks if score wants to be reset
+	        reset = input("Would you like to reset the score? (y/n) ").strip().lower()
+	        while reset != "y" and reset != "n":
+		        print("Invalid response. Please try again.")
+		        reset = input("Would you like to reset the score? (y/n) ").strip().lower()
+	        if reset == "y":
+		        player_win = 0
+		        computer_win = 0
+		        print("Score Reset!")
+		        
         choices = get_choices_remix()
         result, player_win, computer_win = check_win_remix(choices["player"], choices["computer"], player_win, computer_win)
         print(result)
